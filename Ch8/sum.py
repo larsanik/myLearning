4197a06d1dd38fc33213f2ee8fd671dd7e0dee2d
a.ershov <a.ershov@vega-gaz.ru>
2022-11-30 15:08:01 +0300

    30.11.2020 #3

--- Ch8/sum.py
+++ Ch8/sum.py
@@ -12,14 +12,29 @@ def recursive_compute_sum(list):
 
 
 def check_polindrom(wrd):
-    if len(wrd) < 3:
-        return 'Очень короткое слово!'
+    nose = []
+    tail = []
+    for i in range(len(wrd)):
+        nose.append(wrd[i])
+        tail.append(wrd[-(i+1)])
+    if tail == nose:
+        return 'Полиндром'
     else:
-        return 'Похоже на слово!'
+        return 'Не полиндром'
 
+def is_polindrom(word):
+    i = 0
+    j = len(word)-1
+    while i<j:
+        if word[i] != word[j]:
+            return False
+        i = i + 1
+        j = j -1
+    return True
 
 # sums = recursive_compute_sum(marbles)
 # print('Сумма равна', sums)
 
-wrd = 'sыыы'
+wrd = 'tacocat'
 print(check_polindrom(wrd))
+print(is_polindrom(wrd))
