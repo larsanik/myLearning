1fef5fb7a28d3d9344ef209d28b402b99f5b4869
a.ershov <a.ershov@vega-gaz.ru>
2022-12-20 16:37:42 +0300

    20.12.2022 #4

--- Ch9/crazy.py
+++ Ch9/crazy.py
@@ -4,15 +4,28 @@ def make_crazy_lib(filename):
     text = ''
 
     for line in file:
-        text = text + process_line(line)
+        text = text + process_line(line)  # + '\n'
 
     file.close()
 
     return text
 
 
+placeholders = ['ГЛАГОЛ', 'СУЩЕСТВИТЕЛЬНОЕ', 'ПРИЛАГАТЕЛЬНОЕ']
+
+
 def process_line(line):
-    return line
+    processed_line = ''
+
+    words = line.split()
+
+    for word in words:
+        if word in placeholders:
+            answer = input('Введите ' + word + ": ")
+            processed_line = processed_line + answer + ' '
+        else:
+            processed_line = processed_line + word + ' '
+    return processed_line + '\n'
 
 
 def main():
