5014a4e50b57dbd4f679a238fc8e5233bda809ea
a.ershov <a.ershov@vega-gaz.ru>
2023-01-26 15:34:02 +0300

    26.01.2023 #2

---
+++ Ch10/iss.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    response_dictionary = json.loads(response.text)
+    position = response_dictionary['iss_position']
+    print('Координаты МКС: ' +
+          position['latitude'] + ', ' + position['longitude'])
+else:
+    print("Хьюстон, у нас проблема:", response.status_code)
+
+# json_string = '{"first": "Emmet", "last": "Brown", "prefix": "Dr."}'
+#
+# name = json.loads(json_string)
+#
+# print(name)
+#
+# print(name['prefix'], name['first'], name['last'])
+
