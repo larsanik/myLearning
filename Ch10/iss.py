25961a0fca9a4ae7e3ea14f58d865ee2c2c91902
a.ershov <a.ershov@vega-gaz.ru>
2023-01-26 16:05:39 +0300

    26.01.2023 #6

--- Ch10/iss.py
+++ Ch10/iss.py
@@ -2,6 +2,14 @@ import json
 import requests
 import turtle
 
+
+def move_iss(lat, long):
+    global iss
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+
+
 screen = turtle.Screen()
 screen.setup(1000, 500)
 screen.bgpic('earth.gif')
@@ -19,12 +27,16 @@ response = requests.get(url)
 if response.status_code == 200:
     response_dictionary = json.loads(response.text)
     position = response_dictionary['iss_position']
-    print('Координаты МКС: ' +
-          position['latitude'] + ', ' + position['longitude'])
+    # print('Координаты МКС: ' +
+    #       position['latitude'] + ', ' + position['longitude'])
+    lat = float(position['latitude'])
+    long = position['longitude']
+    move_iss(lat, long)
 else:
     print("Хьюстон, у нас проблема:", response.status_code)
 
 turtle.mainloop()
+
 # json_string = '{"first": "Emmet", "last": "Brown", "prefix": "Dr."}'
 #
 # name = json.loads(json_string)
