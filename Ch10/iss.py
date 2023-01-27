5065358902bebf49405ea85f744ccea577103547
a.ershov <a.ershov@vega-gaz.ru>
2023-01-27 11:39:08 +0300

    27.01.2023 #1

--- Ch10/iss.py
+++ Ch10/iss.py
@@ -2,6 +2,18 @@ import json
 import requests
 import turtle
 
+iss = turtle.Turtle()
+
+
+def setup(window):
+    global iss
+
+    window.setup(1000, 500)
+    window.bgpic('earth.gif')
+    window.setworldcoordinates(-180, -90, 180, 90)
+    turtle.register_shape('iss.gif')
+    iss.shape('iss.gif')
+
 
 def move_iss(lat, long):
     global iss
@@ -11,39 +23,28 @@ def move_iss(lat, long):
     iss.pendown()
 
 
-screen = turtle.Screen()
-screen.setup(1000, 500)
-screen.bgpic('earth.gif')
-screen.setworldcoordinates(-180, -90, 180, 90)
+def track_iss():
+    url = 'http://api.open-notify.org/iss-now.json'
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_dictionary = json.loads(response.text)
+        position = response_dictionary['iss_position']
+        lat = float(position['latitude'])
+        long = float(position['longitude'])
+        print(lat, long)
+        move_iss(lat, long)
+    else:
+        print("Хьюстон, у нас проблема:", response.status_code)
+    widget = turtle.getcanvas()
+    widget.after(5000, track_iss)
 
-iss = turtle.Turtle()
-turtle.register_shape('iss.gif')
-iss.shape('iss.gif')
+def main():
+    global iss
+    screen = turtle.Screen()
+    setup(screen)
+    track_iss()
 
 
-url = 'http://api.open-notify.org/iss-now.json'
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    response_dictionary = json.loads(response.text)
-    position = response_dictionary['iss_position']
-    # print('Координаты МКС: ' +
-    #       position['latitude'] + ', ' + position['longitude'])
-    lat = float(position['latitude'])
-    long = float(position['longitude'])
-    print(lat, long)
-    move_iss(lat, long)
-else:
-    print("Хьюстон, у нас проблема:", response.status_code)
-
-turtle.mainloop()
-
-# json_string = '{"first": "Emmet", "last": "Brown", "prefix": "Dr."}'
-#
-# name = json.loads(json_string)
-#
-# print(name)
-#
-# print(name['prefix'], name['first'], name['last'])
-
+if __name__ == "__main__":
+    main()
+    turtle.mainloop()
