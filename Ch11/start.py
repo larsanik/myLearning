7f9aa796f9e63764c555700e2837199fe8043011
a.ershov <a.ershov@vega-gaz.ru>
2023-02-14 16:26:32 +0300

    14.02.2023 #2

---
+++ Ch11/start.py
@@ -0,0 +1,22 @@
+def init():
+    height = 100
+    width = 100
+
+    grid_model = [0] * height
+
+    for i in range(height):
+        grid_model[i] = [0] * width
+
+    print(grid_model)
+
+# def next_gen:
+
+def main():
+    init()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
